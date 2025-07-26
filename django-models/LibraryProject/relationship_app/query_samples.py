6675ee859eedaec37484ffbe3bd9c1f25b778c71
Shobs <shobowaleahmedayobami@gmail.com>
2025-07-26 12:05:17 -1100

    Add query to filter books by specific author

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,10 @@
-# relationship_app/query_samples.py
+from relationship_app.models import Author, Book
 
-from relationship_app.models import Author, Book, Library, Librarian
+# Example: Query all books by a specific author
+author_name = "CharField"  # Replace with actual author name
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
 
-# Query all books by a specific author
-def get_books_by_author(author_name):
-    return Book.objects.filter(author__name=author_name)
-
-# List all books in a library
-def get_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.books.all()
-
-# Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
-    return Librarian.objects.get(library__name=library_name)
+# Print results (optional for debugging)
+for book in books_by_author:
+    print(book)
