662404831c0a26000bf38e681ddc08784c1c2ad9
Shobs <shobowaleahmedayobami@gmail.com>
2025-07-27 01:30:22 -1100

    Add UserProfile model and post_save signals

--- LibraryProject/relationship_app/models.py
+++ LibraryProject/relationship_app/models.py
@@ -1,11 +1,5 @@
-# For checker:
-# class UserProfile(models.Model):
-# Admin
-# Member
-
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 
 from django.contrib.auth.models import User
 from django.db import models
