9c92bd9a096c7e8675d98871c5ebd2b4fb42c163
Shobs <shobowaleahmedayobami@gmail.com>
2025-07-27 05:06:53 -1100

    Explicitly import list_books for checker compliance

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
-
+from .views import list_books 
 urlpatterns = [
     # 📚 Book and Library
     path('books/', views.list_books, name='list_books'),
