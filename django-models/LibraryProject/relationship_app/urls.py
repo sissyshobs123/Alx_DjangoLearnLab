4349f6e5eb5420d9b1510ed2e89f5651bd3559b2
Shobs <shobowaleahmedayobami@gmail.com>
2025-07-27 03:05:59 -1100

    Add role-based views and templates for Admin, Librarian, and Member

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
 
     # 👥 Role-Based Access
-    path('admin-only/', views.admin_view, name='admin_view'),
-    path('librarian-only/', views.librarian_view, name='librarian_view'),
-    path('member-only/', views.member_view, name='member_view'),
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
 ]
