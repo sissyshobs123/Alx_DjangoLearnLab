152e82733fbe0d48daf841b1ec4e239b2fc27383
Shobs <shobowaleahmedayobami@gmail.com>
2025-08-02 00:10:07 -1100

    Fix INSTALLED_APPS typo and run makemigrations

--- advanced_features_and_security/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf', 
+    'LibraryProject.bookshelf',
     'custom_user_app',
 
 ]
