9d77e41e2d8a3e8a349922a1d9dcb844738c7e71
Shobs <shobowaleahmedayobami@gmail.com>
2025-08-03 05:45:23 -1100

    fix: add book_list view with permission check

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponse
+from .models import Book
 
-# Create your views here.
+def is_editor(user):
+    return user.groups.filter(name='Editors').exists()
+
+@user_passes_test(is_editor)
+def editor_only_view(request):
+    return HttpResponse("Welcome, Editor! You have access to this page.")
+
+
+def is_librarian(user):
+    return user.groups.filter(name="Librarians").exists()
+
+@login_required
+@user_passes_test(is_librarian)
+def librarian_dashboard(request):
+    return render(request, 'librarian_dashboard.html')
+
+@permission_required('bookshelf.can_view_books', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
