e16843faad0221f74362d56b9652d15fd444fc9a
Shobs <shobowaleahmedayobami@gmail.com>
2025-08-08 09:01:01 -1100

    Install Django and update project files

---
+++ api_project/api/models.py
@@ -0,0 +1,9 @@
+# Create your models here.
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
