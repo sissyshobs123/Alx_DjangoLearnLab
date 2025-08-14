21b0a475708a19c8e2f3c8f89ac8ce9ab4fa32d2
Shobs <shobowaleahmedayobami@gmail.com>
2025-08-14 03:41:14 -1100

    Task 0: Complete Django blog setup with MySQL database, Post model, migrations, and static/templates files

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog_db',
+        'USER': 'blog_user',
+        'PASSWORD': '@Shobowale123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
