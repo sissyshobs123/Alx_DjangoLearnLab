52a515e561005a5030eb409a60f50d93060caecf
Shobs <shobowaleahmedayobami@gmail.com>
2025-08-13 13:12:44 -1100

    Add books/update and books/delete URL patterns in api/urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,15 +4,10 @@ Each endpoint maps to a specific view handling CRUD operations.
 """
 
 from django.urls import path
-from .views import (
-    BookListView, BookDetailView, BookCreateView,
-    BookUpdateView, BookDeleteView
-)
+from . import views
 
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),  # List all books
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Retrieve a book
-    path('books/create/', BookCreateView.as_view(), name='book-create'),  # Create a book
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  # Update a book
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book
+    path('books/', views.BookListCreateView.as_view(), name='book-list-create'),
+    path('books/update/<int:pk>/', views.BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', views.BookDeleteView.as_view(), name='book-delete'),
 ]
