2a9ba1a17ef01a862b5243bcacfcac1aae1fc1a4
Shobs <shobowaleahmedayobami@gmail.com>
2025-08-13 13:14:20 -1100

    Add books/create URL pattern in api/urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -7,7 +7,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.BookListCreateView.as_view(), name='book-list-create'),
-    path('books/update/<int:pk>/', views.BookUpdateView.as_view(), name='book-update'),
-    path('books/delete/<int:pk>/', views.BookDeleteView.as_view(), name='book-delete'),
+    path('books/create', views.BookCreateView.as_view(), name='book-create'),
+    path('books/update/<int:pk>', views.BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>', views.BookDeleteView.as_view(), name='book-delete'),
 ]
